28e509b12042e2b8e90868719bbc7553466f06c8
leobewater <mmbee888@gmail.com>
2023-12-30 15:03:05 -0500

    change drone speed and add to level

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -1,11 +1,18 @@
 extends CharacterBody2D
 
+#var is_enemy: bool = true
+
+
 func _process(_delta):
 	# direction
 	var direction = Vector2.RIGHT
 	
 	# velocity
-	velocity = direction * 400
+	velocity = direction * 200
 	
 	# move and slide
 	move_and_slide()
+
+
+func hit():
+	print("drone got damage")
