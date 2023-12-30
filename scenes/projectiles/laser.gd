5fc479d63d3d8d3bfe08437b006c34f690e9cd91
leobewater <mmbee888@gmail.com>
2023-12-30 15:03:32 -0500

    when laser collide with anything remove itself and also remove after timer 2s

--- scenes/projectiles/laser.gd
+++ scenes/projectiles/laser.gd
@@ -1,7 +1,33 @@
 extends Area2D
 
+
+@onready var self_destruct_timer = $SelfDestructTimer
+
+
 @export var speed: int = 1000
+
+
 var direction: Vector2 = Vector2.UP
+
+
+func _ready():
+	self_destruct_timer.start()
 
 func _process(delta):
 	position += direction * speed * delta
+
+
+# when laser collides
+func _on_body_entered(body):
+	print("laser collision:", body)
+	# check a method or property is inside the body node
+	if "hit" in body:
+		body.hit()
+	#if "is_enemy" in body:
+		#print("it's an enemy")
+	queue_free()
+
+
+# remove laser itself after 2 seconds
+func _on_self_destruct_timer_timeout():
+	queue_free()
