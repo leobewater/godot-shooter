99acd93a198c7b9ea69afe44b386db4870a1f3ff
leobewater <mmbee888@gmail.com>
2023-12-31 09:27:07 -0500

    use preload instead of loading from file direction

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -1,5 +1,6 @@
 extends LevelParent
 
+var outside_level_scene: PackedScene = preload("res://scenes/levels/outside.tscn")
 
 func _on_exit_gate_area_body_entered(body):
 	print("Player has exited inside level: ", body)
@@ -7,5 +8,5 @@ func _on_exit_gate_area_body_entered(body):
 	# stop player from moving after entering the gate
 	var tween = create_tween()
 	tween.tween_property(player, "speed", 0, 0.5)
-	get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
-	
+	#get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
+	get_tree().change_scene_to_packed(outside_level_scene)
--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -1,5 +1,7 @@
 extends LevelParent
 
+var inside_level_scene: PackedScene = preload("res://scenes/levels/inside.tscn")
+
 
 func _on_gate_player_entered_gate(body):
 	print("Player has entered gate: ", body)
@@ -7,4 +9,5 @@ func _on_gate_player_entered_gate(body):
 	# stop player from moving after entering the gate
 	var tween = create_tween()
 	tween.tween_property(player, "speed", 0, 0.5)
-	get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
+	#get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
+	get_tree().change_scene_to_packed(inside_level_scene)
