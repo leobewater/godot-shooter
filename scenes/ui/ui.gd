577db9bbe5b6a42b8fe466e7448d26514591719e
leobewater <mmbee888@gmail.com>
2024-01-01 08:09:34 -0500

    change label and icon color to red when amount is zero

--- scenes/ui/ui.gd
+++ scenes/ui/ui.gd
@@ -1,8 +1,15 @@
 extends CanvasLayer
 
+# colors
+var green: Color = Color("#6bbfa3")
+var red: Color = Color(0.9, 0, 0, 1)
+
 
 @onready var laser_label: Label = $LaserCounter/VBoxContainer/Label
+@onready var laser_icon = $LaserCounter/VBoxContainer/TextureRect
+
 @onready var grenade_label = $GrenadeCounter/VBoxContainer/Label
+@onready var grenade_icon = $GrenadeCounter/VBoxContainer/TextureRect
 
 
 func _ready():
@@ -12,7 +19,19 @@ func _ready():
 
 func update_laser_text():
 	laser_label.text = str(Globals.laser_amount)
+	update_color(Globals.grenade_amount, laser_label, laser_icon)
 
 
 func update_grenade_text():
 	grenade_label.text = str(Globals.grenade_amount)
+	update_color(Globals.grenade_amount, grenade_label, grenade_icon)
+
+
+func update_color(amount:int, label: Label, icon: TextureRect) -> void:
+	if amount <= 0:
+		label.modulate = red
+		icon.modulate = red
+	else:
+		label.modulate = green
+		icon.modulate = green
+	
