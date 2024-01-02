b562cb55c11a43489bece75db007f86f670176cf
leobewater <mmbee888@gmail.com>
2024-01-02 10:48:19 -0500

    randomly create an item type and modify color

--- scenes/items/item.gd
+++ scenes/items/item.gd
@@ -1,2 +1,18 @@
 extends Area2D
 
+@onready var sprite_2d = $Sprite2D
+
+var rotation_speed: int = 4
+# give laser more chances
+var available_options = ['laser', 'laser', 'laser', 'laser', 'grenade', 'health']
+var type = available_options.pick_random()
+
+
+func _ready():
+	#print(type)
+	if type == 'laser':
+		sprite_2d.modulate = Color(0.1,0.2,0.8)
+
+
+func _process(delta):
+	rotation += rotation_speed * delta
