832bf033f76dacd3ea5c7f5445b93b7952badc17
leobewater <mmbee888@gmail.com>
2024-01-02 10:24:24 -0500

    autoload transition layer to add fading effect when change outside to inside scene

---
+++ globals/transition_layer.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+
+@onready var animation_player = $AnimationPlayer
+
+
+func change_scene(target: String) -> void:
+	animation_player.play("fade_to_black")
+	# wait for animation finished
+	await animation_player.animation_finished
+	get_tree().change_scene_to_file(target)
+	animation_player.play("reveal")
--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -10,4 +10,5 @@ func _on_gate_player_entered_gate(body):
 	var tween = create_tween()
 	tween.tween_property(player, "speed", 0, 0.5)
 	#get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
-	get_tree().change_scene_to_packed(inside_level_scene)
+	#get_tree().change_scene_to_packed(inside_level_scene)
+	TransitionLayer.change_scene("res://scenes/levels/inside.tscn")
