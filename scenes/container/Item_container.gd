c796eef2e003c89fecb34793334f0d6639829fa0
leobewater <mmbee888@gmail.com>
2024-01-02 15:28:23 -0500

    only allow to open create once

--- scenes/container/Item_container.gd
+++ scenes/container/Item_container.gd
@@ -2,9 +2,10 @@ extends StaticBody2D
 
 class_name ItemContainer
 
-signal open(pos, direction)
-
 # run this after the ready() method which already applied the rotation set in the trees
 # so when we spawn the item, it will follow the parent item's rotation
 @onready var current_direction: Vector2 = Vector2.DOWN.rotated(rotation)
 
+var opened: bool = false
+
+signal open(pos, direction)
--- scenes/container/crate.gd
+++ scenes/container/crate.gd
@@ -4,15 +4,14 @@ extends ItemContainer
 @onready var spawn_positions = $SpawnPositions
 
 func hit():
-	#print("crate current_direction: ", current_direction)
-	
-	# when getting shot the lid disappear
-	lid_sprite.hide()
-	
-	# create multiple items
-	for i in range(5):
-		# randomly pick a marker 2d for spawning starting position
-		var pos = spawn_positions.get_children().pick_random().global_position
-		open.emit(pos, current_direction)
+	if not opened:
+		# when getting shot the lid disappear
+		lid_sprite.hide()
+		
+		# create multiple items
+		for i in range(5):
+			# randomly pick a marker 2d for spawning starting position
+			var pos = spawn_positions.get_children().pick_random().global_position
+			open.emit(pos, current_direction)
 
-
+		opened = true
