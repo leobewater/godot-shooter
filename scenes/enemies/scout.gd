b8c4e84c0b86c67d98f5915830fb8e3b40c0da77
leobewater <mmbee888@gmail.com>
2024-01-03 10:35:21 -0500

    make scout vulnerable based on the timer setting

--- scenes/enemies/scout.gd
+++ scenes/enemies/scout.gd
@@ -1,13 +1,17 @@
 extends CharacterBody2D
 
 @onready var laser_spawn_positions = $LaserSpawnPositions
-@onready var laser_timer = $LaserTimer
 @onready var marker_2d = $LaserSpawnPositions/Marker2D
+@onready var laser_timer = $Timers/LaserTimer
+@onready var hit_timer = $Timers/HitTimer
+
 
 var player_nearby: bool = false
 var can_laser: bool = true
 var right_gun_use: bool = true
+
 var health: int = 30
+var vulnerable: bool = true
 
 signal laser(pos, direction)
 
@@ -15,10 +19,13 @@ signal laser(pos, direction)
 # grenade is calling this multiple time per sec
 func hit():
 	print("scout was hit")
-	health -= 10
-	if health <= 0:
-		queue_free()
-	
+	if vulnerable:
+		health -= 10
+		vulnerable = false
+		hit_timer.start()
+		if health <= 0:
+			queue_free()
+		
 	
 func _process(_delta):
 	if player_nearby:
@@ -43,5 +50,9 @@ func _on_attack_area_body_exited(_body):
 	player_nearby = false
 
 
-func _on_laser_cooldown_timeout():
+func _on_laser_timer_timeout():
 	can_laser = true
+
+
+func _on_hit_timer_timeout():
+	vulnerable = true
