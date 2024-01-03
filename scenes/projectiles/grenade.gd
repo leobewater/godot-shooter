1261ada32d362d9a75a139803598a0feb382deae
leobewater <mmbee888@gmail.com>
2024-01-03 09:15:14 -0500

    explode container items within radius

--- scenes/projectiles/grenade.gd
+++ scenes/projectiles/grenade.gd
@@ -3,8 +3,20 @@ extends RigidBody2D
 @onready var animation_player = $AnimationPlayer
 
 const speed = 750
-
+var explosion_active: bool = false
+var explosion_radius: int = 400
 
 # this being called from the blinking animation
 func explode():
 	animation_player.play("explosion")
+	explosion_active = true
+
+
+func _process(_delta):
+	if explosion_active:
+		# hit all targets within the radius when explode
+		var targets = get_tree().get_nodes_in_group("Container")
+		for target in targets:
+			var in_range = target.global_position.distance_to(global_position) < explosion_radius
+			if "hit" in target and in_range:
+				target.hit()
