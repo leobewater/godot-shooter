54d6307a5d3b1b3acc9c753fd5af4925206e2a2a
leobewater <mmbee888@gmail.com>
2024-01-03 15:44:45 -0500

    add line2d in raycast and draw line via code

--- scenes/enemies/car.gd
+++ scenes/enemies/car.gd
@@ -2,9 +2,19 @@ extends PathFollow2D
 
 @onready var turret = $Turret
 
+@onready var ray_cast_1 = $Turret/RayCast2D
+@onready var line1 = $Turret/RayCast2D/Line2D
+
+@onready var ray_cast_2 = $Turret/RayCast2D2
+@onready var line2 = $Turret/RayCast2D2/Line2D
+
 
 var player_near: bool = false
 
+
+func _ready():
+	line2.add_point(ray_cast_2.target_position)
+	
 
 func _process(delta):
 	progress_ratio += 0.01 * delta
