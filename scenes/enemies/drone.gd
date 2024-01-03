28a04b4277dc0de69eebdf07817817fd70fa4a28
leobewater <mmbee888@gmail.com>
2024-01-03 13:50:41 -0500

    make drone moves to player and add damage logic

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -1,18 +1,33 @@
 extends CharacterBody2D
 
-#var is_enemy: bool = true
+var active: bool = false
+var speed: int = 400
 
+@onready var hit_timer = $HitTimer
+
+var vulnerable: bool = true
+var health: int  = 50
 
 func _process(_delta):
-	# direction
-	var direction = Vector2.RIGHT
-	
-	# velocity
-	velocity = direction * 200
-	
-	# move and slide
-	move_and_slide()
+	if active:
+		look_at(Globals.player_pos)
+		var direction = (Globals.player_pos - position).normalized()
+		velocity = direction * speed
+		move_and_slide()
 
 
 func hit():
-	print("drone got damage")
+	if vulnerable:
+		health -= 10
+		vulnerable = false
+		hit_timer.start()
+		print("drone got damage")
+	if health <= 0:
+		queue_free()
+
+func _on_notice_area_body_entered(_body):
+	active = true
+
+
+func _on_hit_timer_timeout():
+	vulnerable = true
