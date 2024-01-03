32a1510a9f18c2e8cc4f21becf4942caef718a34
leobewater <mmbee888@gmail.com>
2024-01-03 12:18:20 -0500

    make bug move to player position

--- scenes/enemies/bug.gd
+++ scenes/enemies/bug.gd
@@ -1,11 +1,29 @@
 extends CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var active: bool = false
+var speed: int = 300
+var vulnerable: bool = true
+var player_near: bool = false
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var direction = (Globals.player_pos - position).normalized()
+	velocity = direction * speed
+	if active:
+		move_and_slide()
+
+
+func _on_attack_area_body_entered(_body):
+	player_near = true
+
+
+func _on_attack_area_body_exited(_body):
+	player_near = false
+
+
+func _on_notice_area_body_entered(_body):
+	active = true
+
+
+func _on_notice_area_body_exited(_body):
+	active = false
