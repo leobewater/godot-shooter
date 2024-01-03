9f446cb19abe27d3617aee9169ce186d1f72b733
leobewater <mmbee888@gmail.com>
2024-01-03 14:33:42 -0500

    speed up drones when seeing player and flash when getting hit

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -6,7 +6,9 @@ extends CharacterBody2D
 @onready var animation_player = $AnimationPlayer
 
 var active: bool = false
-var speed: int = 400
+var max_speed: int = 600
+var speed: int = 0
+var speed_multiplier: int = 1
 var vulnerable: bool = true
 var health: int  = 50
 
@@ -20,7 +22,7 @@ func _process(delta):
 	if active:
 		look_at(Globals.player_pos)
 		var direction = (Globals.player_pos - position).normalized()
-		velocity = direction * speed
+		velocity = direction * speed * speed_multiplier
 		var collision = move_and_collide(velocity * delta)
 		if collision:
 			animation_player.play("explosion")
@@ -32,13 +34,24 @@ func hit():
 		vulnerable = false
 		hit_timer.start()
 		print("drone got damage")
+		sprite_2d.material.set_shader_parameter('progress', 1)
 	if health <= 0:
 		animation_player.play("explosion")
+
+# used in animation
+func stop_movement():
+	speed_multiplier = 0
 
 
 func _on_notice_area_body_entered(_body):
 	active = true
+	# speed up via tween when player enters the area
+	var tween = create_tween()
+	tween.tween_property(self, "speed", max_speed, 6)
 
 
 func _on_hit_timer_timeout():
 	vulnerable = true
+	sprite_2d.material.set_shader_parameter('progress', 0)
+
+
