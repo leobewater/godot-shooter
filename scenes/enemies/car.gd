c2bdd87c6a223636beafea05523997390075f9b7
leobewater <mmbee888@gmail.com>
2024-01-03 15:26:14 -0500

    make car's turret looks at player

--- scenes/enemies/car.gd
+++ scenes/enemies/car.gd
@@ -1,6 +1,20 @@
 extends PathFollow2D
 
+@onready var turret = $Turret
+
+
+var player_near: bool = false
 
 
 func _process(delta):
-	progress_ratio += 0.05 * delta
+	progress_ratio += 0.01 * delta
+	if player_near:
+		turret.look_at(Globals.player_pos)
+
+
+func _on_notice_area_body_entered(body):
+	player_near = true
+
+
+func _on_notice_area_body_exited(body):
+	player_near = false
