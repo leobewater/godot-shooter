6082b314b4d4ec74b4441c9dd649e74b89d5d992
leobewater <mmbee888@gmail.com>
2024-01-03 12:23:38 -0500

    make bug attacks every second

--- scenes/enemies/bug.gd
+++ scenes/enemies/bug.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var attack_timer = $Node/AttackTimer
+@onready var hit_timer = $Node/HitTimer
 
 var active: bool = false
 var speed: int = 300
@@ -34,3 +36,19 @@ func _on_notice_area_body_entered(_body):
 func _on_notice_area_body_exited(_body):
 	active = false
 	animated_sprite_2d.stop()
+
+
+func _on_animated_sprite_2d_animation_finished():
+	# walk animation is loop and never triggered finished
+	# only attack animation finished
+	if player_near:
+		Globals.health -= 10
+		attack_timer.start()
+		
+
+func _on_hit_timer_timeout():
+	pass # Replace with function body.
+
+
+func _on_attack_timer_timeout():
+	animated_sprite_2d.play("attack")
